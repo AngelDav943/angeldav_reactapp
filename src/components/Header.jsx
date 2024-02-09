618584f77af7645ed79ef3026f1c1d6f43d550f9
Angel <angel92.addcs@gmail.com>
2024-02-09 15:23:18 -0500

    header styling, dashboard (profile picture and status edition)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,17 @@ import './header.css'
 import { Link } from "react-router-dom"
 
 
-export default function() {
+export default function () {
     const { info } = useInfo();
-    
+
     const [headerOpen, setHeaderOpen] = useState(false)
-    function NavLink({children, to: target}) {
-        return <Link to={target} onClick={() => {setHeaderOpen(false)}}>{children}</Link>
+    function NavLink({ children, to: target, className }) {
+        return <Link className={className} to={target} onClick={() => { setHeaderOpen(false) }}>{children}</Link>
     }
 
     return (
         <header className={headerOpen ? "open" : ""}>
-            <img src="/favicon.ico" alt="logo" onClick={() => {setHeaderOpen(!headerOpen)}}/>
+            <img src="/favicon.ico" alt="logo" onClick={() => { setHeaderOpen(!headerOpen) }} />
             <div className="left">
                 <nav>
                     <NavLink to={"/"}>Home</NavLink>
@@ -22,8 +22,11 @@ export default function() {
                 </nav>
                 {info == null ? (
                     <NavLink className="submit" to={"/dashboard"}>Login</NavLink>
-                ): (
-                    <NavLink to={"/dashboard"}>{info?.username}</NavLink>
+                ) : (
+                    <NavLink className="profile" to={"/dashboard"}>
+                        <img src={info?.profile} alt="profile" />
+                        <span>{info?.username}</span>
+                    </NavLink>
                 )}
             </div>
         </header>
--- src/components/dash/home.jsx
+++ src/components/dash/home.jsx
@@ -1,9 +1,100 @@
+import { useState } from "react";
 import { useInfo } from "../../context/useInfo";
 
-export default function() {
-    const { info: user } = useInfo();
+import './home.css'
 
-    return <center>
-        <p>Hello {user?.username}</p>
-    </center>
+export default function () {
+    const { info, getData } = useInfo();
+
+    const [profileImage, setProfile] = useState(info?.profile);
+    const [profileStatus, setStatus] = useState(info?.status || "");
+
+    const uploadProfile = async () => {
+        const files = document.getElementById("uploadImage").files;
+        if (files == null || files[0] == null) return;
+
+        if (files[0].type != "image/png" && files[0].type != "image/jpeg") return
+        //console.log(files[0].type)
+
+        var reader = new FileReader();
+        var result = ""
+        reader.onload = function (e) {
+            var image = new Image();
+            image.src = reader.result;
+            
+            image.onload = function () {
+                var canvas = document.createElement("canvas");
+                canvas.width = "64";
+                canvas.height = "64";
+
+                var ctx = canvas.getContext("2d");
+
+                var gradient = ctx.createLinearGradient(0, 0, 64, 64);
+                gradient.addColorStop(0, "#ff8300");
+                gradient.addColorStop(1, "orange");
+                ctx.fillStyle = gradient;
+                ctx.fillRect(0,0, 64, 64)
+
+                ctx.drawImage(image, 0, 0, 64, 64);
+
+                const dataURL = canvas.toDataURL("image/jpeg");
+
+                // max length should be 5000
+                console.log("from", reader.result.length,"to", dataURL.length)
+
+                setProfile(dataURL)
+            }
+        }
+
+        reader.readAsDataURL(files[0])
+    }
+
+    const saveData = async function() {
+        var modifiedData = {}
+
+        if (info?.profile != profileImage) modifiedData["profile"] = profileImage
+        if (info?.status != profileStatus) modifiedData["status"] = profileStatus
+
+        console.log(modifiedData)
+
+        // TODO: send all data to API
+        var fetchedData = await fetch('https://datatest.angelddcs.workers.dev/users', {
+            method: 'PATCH',
+            headers: { "token": info?.token, "Content-Type":"application/json" },
+            body: JSON.stringify(modifiedData)
+        })
+
+        var response = await fetchedData.json().catch(err => {
+            return { msg: String(err) }
+        })
+
+        console.log(response)
+
+        getData();
+    }
+
+    return <article className="dashhome">
+        <input type="file" id="uploadImage" onChange={(e) => uploadProfile(e)} />
+        <br />
+
+        <section className="item">
+            <div className="profile">
+                <img src={profileImage} alt="profile picture" height={75}/>
+                <span>@{info.username}</span>
+            </div>
+            <div className="inputs">
+                <label>
+                    Username
+                    <input placeholder={info?.username} disabled={true} />
+                </label>
+                <br />
+                <label>
+                    Status
+                    <textarea name="" id="" cols="30" defaultValue={profileStatus} />
+                </label>
+            </div>
+        </section>
+
+        <input type="submit" value="Save" onClick={() => saveData()}/>
+    </article>
 }
