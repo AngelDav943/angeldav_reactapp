6250ed584f7ecad418509d1c8517bbbf359617d3
Angel <angel92.addcs@gmail.com>
2024-06-02 18:11:39 -0500

    moved info button on gallery card

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useInfo } from '../context/useInfo'
 import { Link } from "react-router-dom"
 
 import './footer.css'
 
 export default function () {
+
 	return <footer>
-		<div className="links">
-			<Link to="/directory" >Directory</Link>
-			<Link to="/statistics" >Statistics</Link>
-			<Link to="https://github.com/AngelDav943" >Github</Link>
+		<div className="row">
+			<div className="links">
+				<Link to="/directory" >Directory</Link>
+				<Link to="/statistics" >Statistics</Link>
+				<Link to="https://github.com/AngelDav943" >Github</Link>
+			</div>
+			<span>Last updated: June 2nd 2024</span>
 		</div>
 		<div className="row">
 			<p className="icon" >Angel's website</p>
